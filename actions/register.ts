c07f9ef194dcc70ca38414bfd439d7f332cf1784
BlackCat <neko.sword@gmail.com>
2024-01-08 15:04:04 +0700

    add: prisma database for feature create user

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod"
 import { RegisterSchema } from "@/schemas"
+import bcrypt from "bcryptjs"
+import { db } from "@/lib/db"
+import { getUserByEmail } from "@/data/user"
 
 export default async function register(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -9,6 +12,22 @@ export default async function register(values: z.infer<typeof RegisterSchema>) {
   if (!validatedFields.success) {
     return { error: "Invalid fields!" }
   }
+
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const existingUser = await getUserByEmail(email)
+
+  if(existingUser) return { error: "Email already in use!!!" }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    }
+  })
+
+  // TODO: Send verification token email
 
   return { success: "Create account success!" }
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,28 @@
-
-import bandner from "../public/tekken8_band.jpg"
+import bandner from "../public/tekken8_band.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-
 const Navbar = async () => {
+  return (
+    <nav className="flex justify-between items-center sticky top-0 w-full mx-auto px-8 py-6 bg-gradient-to-b from-black from-80% via-slate-950 via-10%">
+      <Link href="/">
+        <Image
+          src={`${bandner.src}`}
+          width={750}
+          height={200}
+          alt="Bandner"
+          style={{ width: "350px", height: "100px" }}
+        />
+      </Link>
 
-	return (
-		<nav className="flex justify-between items-center sticky top-0 w-full mx-auto px-8 py-6 bg-gradient-to-b from-black from-80% via-slate-950 via-10%">
-			<Link href="/">
-				<Image
-					src={`${bandner.src}`}
-					width={750}
-					height={200}
-					alt="Bandner"
-					style={{ width: "350px", height: "100px" }}
-				/>
-			</Link>
-			<div className="sm:text-2xl">
-				Welcome
-			</div>
-				<Link href="/api/auth/signout?callbackUrl=/">
-					<button 
-						className="text-rose-700 hover:text-white border hover:border-none rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-rose-900 duration-300 px-4 py-2 lg:px-8 lg:py-4 font-medium sm:text-2xl"
-					>
-						Log out
-					</button>
-				</Link>
-		</nav>
-	);
+      <Link href="/api/auth/signout?callbackUrl=/">
+        <button className="text-rose-700 hover:text-white border hover:border-none rounded-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-rose-900 duration-300 px-4 py-2 lg:px-8 lg:py-4 font-medium sm:text-2xl">
+          Log out
+        </button>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
